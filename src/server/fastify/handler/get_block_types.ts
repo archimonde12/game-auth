53ec8da5e68ec17fd658c1b755eb32a32ee19853
Hoan Luu <hoanluu@Hoans-Mac-mini.local>
2022-01-25 15:31:45 +0700

    update link image

--- src/server/fastify/handler/get_block_types.ts
+++ src/server/fastify/handler/get_block_types.ts
@@ -28,7 +28,7 @@ const BLOCK_TYPES: BlockType[] = [
         faceType: 2,
         icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVj8xhQsqWYRkmcZ0r0V9KzxQl8qXTM35-uQ&usqp=CAU",
         faces: [{
-            texture: "https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/454c9a9174136b1.png",
+            texture: "https://media.istockphoto.com/vectors/subtle-halftone-dots-vector-texture-overlay-vector-id1163666619?k=20&m=1163666619&s=612x612&w=0&h=f07B-2n5plROeW6Vx3cVbRw00_BuXQLZxRX_IdsR6mo=",
             normals: "https://i.pinimg.com/564x/b3/d2/fd/b3d2fd54a8a44c665fd09bb6f4b7feb8--d-artist-broken-glass.jpg",
             alpha: 0.5
         }],
@@ -42,7 +42,7 @@ const BLOCK_TYPES: BlockType[] = [
         faceType: 2,
         icon: "https://image.shutterstock.com/image-vector/ground-cube-isolated-260nw-702797671.jpg",
         faces: [{
-            texture: "https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/0c798ef36649f8c.png",
+            texture: "https://media.istockphoto.com/photos/earth-texture-macro-photography-pattern-background-picture-id1277752567?k=20&m=1277752567&s=612x612&w=0&h=fHnOc6HznYZreHs5baH5e2448U-Eua6to66qoHf05Ds=",
             normals: "https://www.researchgate.net/profile/Andreas-Junker-3/publication/344587438/figure/fig4/AS:989314339074049@1612882327388/From-left-to-right-Wind-map-Texture-and-Normal-map-The-wind-map-is-a-texture-composed.ppm",
             alpha: 0.5
         }],
@@ -56,7 +56,7 @@ const BLOCK_TYPES: BlockType[] = [
         faceType: 2,
         icon: "https://cdn2.iconfinder.com/data/icons/cube-three/64/Shattered_stone-512.png",
         faces: [{
-            texture: "https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d02578b45806cd9.png",
+            texture: "https://media.istockphoto.com/photos/light-grey-black-slate-stone-background-or-texture-picture-id1164746310?k=20&m=1164746310&s=170667a&w=0&h=2zmUKpxexjSPER20VtR0spCcYtVf8cTYwK2NgF2iCfU=",
             normals: "https://thumbs.dreamstime.com/b/r-stone-wall-normal-map-computer-generated-texture-masonry-differently-sized-stones-insert-concrete-seamless-tileable-repeating-99018769.jpg",
             alpha: 0.5
         }],
